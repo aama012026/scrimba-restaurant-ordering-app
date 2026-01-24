b511601a3f28e7517d759c257f344a305f2e84ca
aama012026 <aama012026@gmail.com>
2026-01-24 12:00:38 +0100

    fixed algorithmic rendering of menu items.

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
-import { menuArray } from "./assets/product-data";
+import { menuArray } from "./assets/product-data.js";
 const productMenu = document.getElementById('product-menu');
+const productTemplate = document.getElementById('product-template');
+
+console.log(productTemplate)
 
 function makeMenuItemHtml(menuItem) {
 	const {id, name, emoji, ingredients, price} = menuItem;
-	const menuItemElement = document.createElement('li');
+	const menuItemElement = document.importNode(productTemplate.content, true);
+	menuItemElement.querySelector('.img').textContent = emoji;
+	menuItemElement.querySelector('.name').textContent = name;
+	menuItemElement.querySelector('.ingredients').textContent = ingredients.join(', ');
+	menuItemElement.querySelector('.price').textContent = price;
+	return menuItemElement;
 }
+
+function renderMenuItems(itemArray) {
+	itemArray.forEach(item => {
+		productMenu.appendChild(makeMenuItemHtml(item));
+	});
+}
+
+renderMenuItems(menuArray);
