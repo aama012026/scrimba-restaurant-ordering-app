6b2cee2536fa3c4c8d7ada49de17f339bf274eb3
aama012026 <aama012026@gmail.com>
2026-01-26 13:19:07 +0100

    Added and styled checkout mockup (to be templated). Adapted style selectors accordingly.

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ import { menuArray } from "./assets/product-data.js";
 const productMenu = document.getElementById('product-menu');
 const productTemplate = document.getElementById('product-template');
 
+// Infrastrukturen for å converte er ikkje laga,
+// men det gir mening å separere representasjon frå verdi.
+let currency = '$'
+
 console.log(productTemplate)
 
 function makeMenuItemHtml(menuItem) {
@@ -10,13 +14,20 @@ function makeMenuItemHtml(menuItem) {
 	menuItemElement.querySelector('.img').textContent = emoji;
 	menuItemElement.querySelector('.name').textContent = name;
 	menuItemElement.querySelector('.ingredients').textContent = ingredients.join(', ');
-	menuItemElement.querySelector('.price').textContent = price;
+	menuItemElement.querySelector('.price').textContent = currency + price;
 	return menuItemElement;
 }
 
 function renderMenuItems(itemArray) {
-	itemArray.forEach(item => {
+	const horizontalRule = document.createElement('hr');
+	horizontalRule.classList.add('slim');
+	itemArray.forEach((item, i) => {
 		productMenu.appendChild(makeMenuItemHtml(item));
+		console.log(i);
+		console.log(itemArray.length);
+		if (i != itemArray.length-1) {
+			productMenu.appendChild(horizontalRule.cloneNode(true));
+		}
 	});
 }
 
